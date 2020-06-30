8455a266a023d42cf46c6f264c7fc0d12c5c4f94
Josh Henke <josh@watchtower.ai>
2020-06-29 18:06:53 -0700

    REmove stuff

--- main.go
+++ main.go
@@ -1,24 +1,6 @@
 package main
 
-import (
-	"github.com/watchtowerai/diff_reviewer/internal/clients"
-	"go.uber.org/fx"
-	"go.uber.org/zap"
-)
-
 // main starts the service process.
 func main() {
-	app := fx.New(
-		fx.Provide(
-			// general constructors
-			newSugarLogger,
-		),
-		clients.Module,
-	)
-	app.Run()
-}
 
-// newSugarLogger helper
-func newSugarLogger(zapLog *zap.Logger) *zap.SugaredLogger {
-	return zapLog.Sugar()
 }

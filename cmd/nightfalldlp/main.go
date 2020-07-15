d4fde7ed52ea9be024ad4458f56567775b034110
Bradley McAllister <brad.mcallister@hotmail.com>
2020-07-15 10:25:11 -0700

    CF-56 Add general create client function (#8)
    * add general create client func
    
    * move content to main file
    
    * call func in main
    
    * pr comments

--- cmd/nightfalldlp/main.go
+++ cmd/nightfalldlp/main.go
@@ -1,8 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/watchtowerai/nightfall_dlp/internal/clients/diffreviewer"
+	"github.com/watchtowerai/nightfall_dlp/internal/clients/diffreviewer/github"
+)
+
+const (
+	githubActionsEnvVar = "GITHUB_ACTIONS"
+)
 
 // main starts the service process.
 func main() {
+	_, err := CreateDiffReviewerClient(&http.Client{})
+	if err != nil {
+		fmt.Printf("Error Getting Client %v", err)
+	}
 	fmt.Println("Running NightfallDLP Action")
+}
+
+// usingGithubAction determine if nightfalldpl is being run by
+// Github Actions
+func usingGithubAction() bool {
+	val, ok := os.LookupEnv(githubActionsEnvVar)
+	if !ok {
+		return false
+	}
+	return val == "true"
+}
+
+// CreateDiffReviewerClient determines the current environment that is running nightfalldlp
+// and returns the corresponding DiffReviewer client
+func CreateDiffReviewerClient(httpClient *http.Client) (diffreviewer.DiffReviewer, error) {
+	switch {
+	case usingGithubAction():
+		return github.NewGithubClient(httpClient), nil
+	default:
+		return nil, errors.New("Current environment unknown")
+	}
 }

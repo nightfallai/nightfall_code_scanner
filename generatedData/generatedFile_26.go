ee80353dc29185dbb6c869a9b6b2e24b526b3362
bradleymcallister97 <brad.mcallister@hotmail.com>
2020-08-03 14:47:23 -0700

    add test files

---
+++ generatedData/generatedFile_26.go
@@ -0,0 +1,170 @@
+This is 309 bytes. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long.
+here is my credit card num 4111 1111 1111 1111 use wisely
+This is a string
+Hello world
+614-124-6522
+This is 309 bytes. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long.
+614-124-6522
+	
+613-124-6522
+614-124-6522
+
+
+This is a string
+This is a string
+This is a string
+613-124-6522
+613-124-6522
+
+
+4916-6734-7572-5015
+This is 309 bytes. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long.
+Hello world
+
+
+there is a phone number in here 613-444-1234
+This is a string
+613-124-6522
+
+
+This is a string
+613-124-6522
+	
+This is a string
+	
+
+
+613-124-6522
+Hello world
+	
+613-124-6522
+
+
+613-404-6522
+there is a phone number in here 613-444-1234
+	
+This is 309 bytes. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long.
+Hello world
+	
+This is a string
+This is 309 bytes. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long.
+Hello world
+
+
+	
+
+
+Hello world
+	
+	
+
+
+	
+	
+there is a phone number in here 613-444-1234
+Hello world
+Hello world
+This is 309 bytes. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long.
+613-124-6522
+This is 309 bytes. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long.
+This is a string
+This is 309 bytes. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long.
+4916-6734-7572-5015
+here is my credit card num 4111 1111 1111 1111 use wisely
+614-124-6522
+Hello world
+4916-6734-7572-5015
+
+
+there is a phone number in here 613-444-1234
+Hello world
+This is a string
+This is 309 bytes. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long.
+This is 309 bytes. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long.
+This is 309 bytes. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long.
+This is 309 bytes. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long.
+613-124-6522
+Hello world
+Hello world
+
+
+This is a string
+Hello world
+4916-6734-7572-5015
+Hello world
+This is a string
+Hello world
+
+
+This is 309 bytes. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long.
+614-124-6522
+	
+Hello world
+
+
+
+
+This is 309 bytes. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long.
+This is a string
+
+
+This is a string
+4916-6734-7572-5015
+This is 309 bytes. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long.
+there is a phone number in here 613-444-1234
+This is a string
+
+
+
+
+This is a string
+Hello world
+4916-6734-7572-5015
+This is a string
+613-124-6522
+613-124-6522
+This is a string
+This is 309 bytes. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long.
+here is my credit card num 4111 1111 1111 1111 use wisely
+4916-6734-7572-5015
+614-124-6522
+	
+Hello world
+there is a phone number in here 613-444-1234
+	
+there is a phone number in here 613-444-1234
+there is a phone number in here 613-444-1234
+This is a string
+
+
+This is 309 bytes. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long.
+Hello world
+
+
+here is my credit card num 4111 1111 1111 1111 use wisely
+there is a phone number in here 613-444-1234
+613-124-6522
+Hello world
+This is 309 bytes. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long.
+Hello world
+Hello world
+This is a string
+613-404-6522
+there is a phone number in here 613-444-1234
+
+
+4916-6734-7572-5015
+This is a string
+613-124-6522
+This is a string
+there is a phone number in here 613-444-1234
+This is 309 bytes. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long.
+This is 309 bytes. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long.
+	
+This is 309 bytes. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long. This is a long line with nothing in it but a bunch of words, but it is very very long long long.
+This is a string
+This is a string
+
+
+This is a string
+Hello world

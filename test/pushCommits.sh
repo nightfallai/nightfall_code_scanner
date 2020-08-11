1b38832fde94bcaccc35f8f18202f267f31f54aa
Alan Yan <alan@nightfall.ai>
2020-08-11 00:48:54 -0400

    update timeout

--- test/pushCommits.sh
+++ test/pushCommits.sh
@@ -3,7 +3,7 @@ do
    # your-unix-command-here
    echo $i
    filename=$( printf 'text/%d.txt' $i )
-   echo 'This is a test $filename my phone num is 301-123-4728' > $filename
+   echo 'This is a test $filename' > $filename
    git add . -A
    git commit -m "$filename"
    git push

3af3a7bbf92cfbdf3b2e418653c5da75864ac300
bradleymcallister97 <brad.mcallister@hotmail.com>
2020-08-18 05:59:44 -0700

    use head

--- internal/clients/gitdiff/gitdiff.go
+++ internal/clients/gitdiff/gitdiff.go
@@ -36,7 +36,7 @@ func (gd *GitDiff) GetDiff() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	diffCmd = exec.Command("git", "show", gd.BaseSHA)
+	diffCmd = exec.Command("git", "show", gd.Head)
 	// diffCmd = exec.Command("git", "show")
 	// diffCmd = exec.Command("git", "diff", "HEAD^", "HEAD")
 	// } else {

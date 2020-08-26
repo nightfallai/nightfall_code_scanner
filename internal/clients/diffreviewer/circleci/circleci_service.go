3afd29962a628d59587288f66149ae714245eadd
Alan Yan <alan@nightfall.ai>
2020-08-26 13:53:58 -0400

    NIGH-110 Circle Service Load Config + Logger (#35)
    * add circle service load config and logger
    
    * add logger tests
    
    * lower case error message
    
    * pr updates
    
    * fix condition, add missing api key test
    
    * rm check for prev commit sha, which can be empty
    
    * rm unused ok var

---
+++ internal/clients/diffreviewer/circleci/circleci_service.go
@@ -0,0 +1,102 @@
+package circleci
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/nightfallai/nightfall_code_scanner/internal/clients/diffreviewer"
+	"github.com/nightfallai/nightfall_code_scanner/internal/clients/gitdiff"
+	"github.com/nightfallai/nightfall_code_scanner/internal/clients/logger"
+	circlelogger "github.com/nightfallai/nightfall_code_scanner/internal/clients/logger/circle_logger"
+	"github.com/nightfallai/nightfall_code_scanner/internal/clients/nightfall"
+	"github.com/nightfallai/nightfall_code_scanner/internal/interfaces/gitdiffintf"
+	"github.com/nightfallai/nightfall_code_scanner/internal/nightfallconfig"
+)
+
+const (
+	WorkspacePathEnvVar          = "GITHUB_WORKSPACE"
+	NightfallAPIKeyEnvVar        = "NIGHTFALL_API_KEY"
+	CircleRepoNameEnvVar         = "CIRCLE_PROJECT_REPONAME"
+	CircleOwnerNameEnvVar        = "CIRCLE_PROJECT_USERNAME"
+	CircleCurrentCommitShaEnvVar = "CIRCLE_SHA1"
+	CircleBeforeCommitEnvVar     = "EVENT_BEFORE"
+	CirclePullRequestUrlEnvVar   = "CIRCLE_PULL_REQUEST"
+)
+
+// Service contains the github client that makes Github api calls
+type Service struct {
+	Logger  logger.Logger
+	GitDiff gitdiffintf.GitDiff
+}
+
+// NewCircleCiService creates a new CircleCi service
+func NewCircleCiService() diffreviewer.DiffReviewer {
+	return &Service{
+		Logger: circlelogger.NewDefaultCircleLogger(),
+	}
+}
+
+// GetLogger gets the github service logger
+func (s *Service) GetLogger() logger.Logger {
+	return s.Logger
+}
+
+// LoadConfig gets all config values from files or environment and creates a config
+func (s *Service) LoadConfig(nightfallConfigFileName string) (*nightfallconfig.Config, error) {
+	s.Logger.Debug("Loading configuration")
+	workspacePath, ok := os.LookupEnv(WorkspacePathEnvVar)
+	if !ok || workspacePath == "" {
+		s.Logger.Error(fmt.Sprintf("Environment variable %s cannot be found", WorkspacePathEnvVar))
+		return nil, errors.New("missing env var for workspace path")
+	}
+	commitSha, ok := os.LookupEnv(CircleCurrentCommitShaEnvVar)
+	if !ok || commitSha == "" {
+		s.Logger.Error(fmt.Sprintf("Environment variable %s cannot be found", CircleCurrentCommitShaEnvVar))
+		return nil, errors.New("missing env var for commit sha")
+	}
+	beforeCommitSha, _ := os.LookupEnv(CircleBeforeCommitEnvVar)
+	s.GitDiff = &gitdiff.GitDiff{
+		WorkDir:    workspacePath,
+		BaseBranch: "master",
+		BaseSHA:    beforeCommitSha,
+		Head:       commitSha,
+	}
+	nightfallConfig, err := nightfallconfig.GetNightfallConfigFile(workspacePath, nightfallConfigFileName)
+	if err != nil {
+		s.Logger.Error("Error getting Nightfall config file. Ensure you have a Nightfall config file located in the root of your repository at .nightfalldlp/config.json with at least one Detector enabled")
+		return nil, err
+	}
+	nightfallAPIKey, ok := os.LookupEnv(NightfallAPIKeyEnvVar)
+	if !ok || nightfallAPIKey == "" {
+		s.Logger.Error(fmt.Sprintf("Error getting Nightfall API key. Ensure you have %s set in the Github secrets of the repo", NightfallAPIKeyEnvVar))
+		return nil, errors.New("missing env var for nightfall api key")
+	}
+
+	var maxNumberRoutines int
+	if nightfallConfig.MaxNumberRoutines < nightfall.MaxConcurrentRoutinesCap {
+		maxNumberRoutines = nightfallConfig.MaxNumberRoutines
+	} else {
+		maxNumberRoutines = nightfall.MaxConcurrentRoutinesCap
+	}
+	return &nightfallconfig.Config{
+		NightfallAPIKey:            nightfallAPIKey,
+		NightfallDetectors:         nightfallConfig.Detectors,
+		NightfallMaxNumberRoutines: maxNumberRoutines,
+		TokenExclusionList:         nightfallConfig.TokenExclusionList,
+		FileInclusionList:          nightfallConfig.FileInclusionList,
+		FileExclusionList:          nightfallConfig.FileExclusionList,
+	}, nil
+}
+
+// GetDiff retrieves the file diff from the requested pull request
+func (s *Service) GetDiff() ([]*diffreviewer.FileDiff, error) {
+	//TODO: implement
+	return nil, nil
+}
+
+// WriteComments posts the findings as annotations to the github check
+func (s *Service) WriteComments(comments []*diffreviewer.Comment) error {
+	//TODO: implement
+	return nil
+}
--- internal/clients/diffreviewer/diffutils/diff_parser.go
+++ internal/clients/diffreviewer/diffutils/diff_parser.go
@@ -1,4 +1,4 @@
-package github
+package diffutils
 
 import (
 	"bufio"

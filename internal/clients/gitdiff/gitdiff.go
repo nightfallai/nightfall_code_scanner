e8dd57335d06b4c20dbd5d1d41dc8320b33f5f4e
Alan Yan <alan@nightfall.ai>
2021-04-23 12:46:11 -0400

    add git logs

--- internal/clients/gitdiff/gitdiff.go
+++ internal/clients/gitdiff/gitdiff.go
@@ -37,11 +37,40 @@ func (gd *GitDiff) GetDiff() (string, error) {
 		// PR event so get diff between base branch and current commit SHA
 		logger.Info("Getting Diff between Base Branch and Current Commit SHA")
 		//err = exec.Command("git", "fetch", "origin", gd.BaseBranch, "--depth=1").Run()
-		err = exec.Command("git", "-c", "http.sslVerify=false", "fetch", "origin", gd.BaseBranch, "--depth=1").Run()
+		fetchCmd := exec.Command(
+			"GIT_TRACE=true",
+			"GIT_CURL_VERBOSE=true",
+			"GIT_SSH_COMMAND=\"ssh -vvv\"\t",
+			"GIT_TRACE_SHALLOW=true",
+			"git", "-c", "http.sslVerify=false", "fetch", "origin", gd.BaseBranch, "--depth=1")
+		reader, err := fetchCmd.StdoutPipe()
+		if err != nil {
+			logger.Error(fmt.Sprintf("Error piping git fetch cmd: %s", err.Error()))
+			return "", err
+		}
+		defer reader.Close()
+		err = fetchCmd.Start()
+		if err != nil {
+			logger.Error(fmt.Sprintf("Error starting git diff cmd: %s", err.Error()))
+			return "", nil
+		}
+		buf := new(strings.Builder)
+		_, err = io.Copy(buf, reader)
+		if err != nil {
+			logger.Error(fmt.Sprintf("Error copying git diff output: %s", err.Error()))
+			return "", err
+		}
+		err = fetchCmd.Wait()
+		if err != nil {
+			logger.Error(fmt.Sprintf("Error waiting for git diff cmd to exit: %s", err.Error()))
+			return "", err
+		}
+		logger.Info(fmt.Sprintf("Fetch Origin Logs: %s", buf.String()))
+		/*err = exec.Command("git", "-c", "http.sslVerify=false", "fetch", "origin", gd.BaseBranch, "--depth=1").Run()
 		if err != nil {
 			logger.Error(fmt.Sprintf("Error getting diff between Base Branch and Current Commit SHA: %s", err.Error()))
 			return "", err
-		}
+		}*/
 		diffCmd = exec.Command("git", "diff", fmt.Sprintf("origin/%s", gd.BaseBranch))
 	case gd.BaseSHA == "" || gd.BaseSHA == unknownCommitHash:
 		// PUSH event for new branch so use git show to get the diff of the most recent commit
@@ -58,11 +87,40 @@ func (gd *GitDiff) GetDiff() (string, error) {
 		// use current commit SHA and previous action run commit SHA for diff
 		logger.Info("Getting Diff for new push event")
 		//err = exec.Command("git", "fetch", "origin", gd.BaseSHA, "--depth=1").Run()
-		err = exec.Command("git", "-c", "http.sslVerify=false", "fetch", "origin", gd.BaseSHA, "--depth=1").Run()
+		fetchCmd := exec.Command(
+			"GIT_TRACE=true",
+			"GIT_CURL_VERBOSE=true",
+			"GIT_SSH_COMMAND=\"ssh -vvv\"\t",
+			"GIT_TRACE_SHALLOW=true",
+			"git", "-c", "http.sslVerify=false", "fetch", "origin", gd.BaseSHA, "--depth=1")
+		reader, err := fetchCmd.StdoutPipe()
 		if err != nil {
-			logger.Error(fmt.Sprintf("Error getting diff in push event: %s", err.Error()))
+			logger.Error(fmt.Sprintf("Error piping git fetch cmd: %s", err.Error()))
 			return "", err
 		}
+		defer reader.Close()
+		err = fetchCmd.Start()
+		if err != nil {
+			logger.Error(fmt.Sprintf("Error starting git diff cmd: %s", err.Error()))
+			return "", nil
+		}
+		buf := new(strings.Builder)
+		_, err = io.Copy(buf, reader)
+		if err != nil {
+			logger.Error(fmt.Sprintf("Error copying git diff output: %s", err.Error()))
+			return "", err
+		}
+		err = fetchCmd.Wait()
+		if err != nil {
+			logger.Error(fmt.Sprintf("Error waiting for git diff cmd to exit: %s", err.Error()))
+			return "", err
+		}
+		logger.Info(fmt.Sprintf("Fetch Origin Logs: %s", buf.String()))
+
+		/*if err != nil {
+			logger.Error(fmt.Sprintf("Error fetching diff in push event: %s", err.Error()))
+			return "", err
+		}*/
 		diffCmd = exec.Command("git", "diff", gd.BaseSHA, gd.Head)
 	}
 
